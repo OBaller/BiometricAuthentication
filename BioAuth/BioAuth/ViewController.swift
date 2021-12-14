7eef0648aa85e219c4300c40a0e7394928c88b03
brightsummit.co <naseemoyebola@gmail.com>
2021-12-14 12:54:25 +0100

    Evaluate BiometricAuthPolicy

--- BioAuth/BioAuth/ViewController.swift
+++ BioAuth/BioAuth/ViewController.swift
@@ -18,9 +18,35 @@ class ViewController: UIViewController {
         context.localizedCancelTitle = "End Session"
         context.localizedFallbackTitle = " Use passcode (2)"
         context.localizedReason = " The app needs your authentication."
-        
+        evaluatePolicy()
     }
-
+    
+    func evaluatePolicy() {
+        var errorCanEval: NSError?
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &errorCanEval) {
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Fallback title - override reasoon") { (success, error) in
+                print(success)
+                if let err = error {
+                    let evalErrCode = LAError(_nsError: err as NSError)
+                    switch evalErrCode.code {
+                        case LAError.Code.userCancel:
+                            print("user cancelled")
+                        case LAError.Code.userFallback:
+                            print("fallback")
+                        case LAError.Code.authenticationFailed:
+                            print("failed")
+                        default:
+                            print("other error")
+                    }
+                }
+                    
+            }
+        }
+        else {
+            print("Can't evaluate")
+            print(errorCanEval?.localizedDescription ?? "no error desc")
+        }
+    }
 
 }
 
